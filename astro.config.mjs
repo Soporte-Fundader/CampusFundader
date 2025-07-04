1121bbeec48cb237326a401165c190a099455e7c
Soporte Fundader <soporte@fundader.org>
2025-07-04 09:06:11 -0500

    Co-authored-by: Vladimir Valladares <vladimir3006@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: node({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://fundader.org',
+  base: '/hubcursos',
+  trailingSlash: 'always',
 });
